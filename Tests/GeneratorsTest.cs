7592780f1499dfb88aa023407936e6ad2034ff54
Alan Alves Luiz <alan.luiz@creasp.org.br>
2022-02-12 19:28:52 -0300

    validation of source code c# compilation

--- Tests/GeneratorsTest.cs
+++ Tests/GeneratorsTest.cs
@@ -3,43 +3,65 @@ using Contracts.Interfaces;
 using NUnit.Framework;
 using Services;
 using Services.Coder;
+using Models;
+using System.Collections.Generic;
 
 namespace Tests
 {
-    [TestFixture]
-    public class GeneratorsTest
-    {
-        IClassDefinition? _classDefiner;
-        IMethodDefinition? _methodDefinition;
-        [SetUp]
-        public void Setup()
-        {
-            _classDefiner = new ClassDefinition(new BuilderClassDefinition());
-            _methodDefinition = new MethodDefinition(new BuilderMethodDefinition());
-        }
+	[TestFixture]
+	public class GeneratorsTest
+	{
+		IClassDefinition? _classDefiner;
+		IMethodDefinition? _methodDefinition;
+		[SetUp]
+		public void Setup()
+		{
+			_classDefiner = new ClassDefinition(new BuilderClassDefinition());
+			_methodDefinition = new MethodDefinition(new BuilderMethodDefinition());
+		}
 
 
-        [Test]
-        public void ClassBusinnesLayer_ReturnStringClassCode()
-        {
-            //arrange
-            var imports = new string[] { "Contracts", "System" }.ToImmutableList();
-            var inheritages = new string[] { "Test", "IInter<Generic>" }.ToImmutableList();
-            var businnessLayerService = _classDefiner!.Builder
-                .Imports(imports)
-                .Namespace("Services")
-                .Name("Test")
-                .Inheritance(inheritages)
-                .Create();
+		[Test]
+		public void ClassBusinnesLayer_ReturnStringClassCode()
+		{
+			var Parameters = new List<Parameter>();
+			Parameters.Add(
+				new Parameter
+				{
+					Name = "message",
+					Type = "string"
+				}
+			);
 
-            //act
-            string result = businnessLayerService.ClassCode;
+			var Method = _methodDefinition!.Builder
+			.Name("Write")
+			.Parameters(Parameters.ToImmutableList())
+			.LogiContent(@" Console.WriteLine($""you said '{message}!'"");")
+			.Create();
 
-            //assert
-            Assert.IsTrue(result.Contains("class"));
-            Assert.IsTrue(result.Contains("}"));
-            Assert.IsTrue(result.Contains("using Contracts"));
-        }
-        
-    }
+			var Methods = new List<IMethodDefinition>();
+			Methods.Add(Method);
+
+			//arrange
+			var imports = new string[] { "System", "Contracts" }.ToImmutableList();
+			var businnessLayerService = _classDefiner!.Builder
+				.Imports(imports)
+				.Namespace("RoslynCompileSample")
+				.Name("Writer")
+                .Methods(Methods.ToImmutableList())
+				.Create();
+
+			//act
+			string result = businnessLayerService.ClassCode;
+
+			var validation = CSharpCompiler.ValidateSourceCode(result);
+
+
+			Assert.IsTrue(validation);
+			//assert
+			Assert.IsTrue(result.Contains("class"));
+			Assert.IsTrue(result.Contains("}"));
+		}
+
+	}
 }
