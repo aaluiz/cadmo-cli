eda728e113baf826c7a77770a044efe7d075acda
Alan Alves Luiz <alan.luiz@creasp.org.br>
2022-03-25 09:15:08 -0300

    update

--- Services/Commands/GenerateModelScriptPartialClasses/FileCodeGenerators.cs
+++ Services/Commands/GenerateModelScriptPartialClasses/FileCodeGenerators.cs
@@ -1,136 +1,145 @@
-using System.Collections.Immutable;
 using Models;
+using System.Collections.Immutable;
 
 namespace Services.Commands
 {
-	public partial class GenerateModelByScript
-	{
-		private FileCode GenerateViewModel(string modelName)
-		{
-			ModelJson modelJson = GetContentJsonFile(modelName);
+    public partial class GenerateModelByScript
+    {
+        private FileCode GenerateViewModel(string modelName)
+        {
+            ModelJson modelJson = GetContentJsonFile(modelName);
 
-			var propertiesFromJson = modelJson.Model!.Fields!.Where(x => x.ViewModelVisibility).Where(y => y.ForeignKey == null).Select(field =>
-			{
-				return ProcessFieldViewModel(field);
-			}).ToList();
+            var propertiesFromJson = modelJson.Model!.Fields!.Where(x => x.ViewModelVisibility).Where(y => y.ForeignKey == null).Select(field =>
+            {
+                return ProcessFieldViewModel(field);
+            }).ToList();
 
-			var properties = new Property[]{
-			new Property{
-				Name = "Id",
-				Visibility = Visibility.Public,
-				hasGeterAndSeter = true,
-				TypeProperty = "int"
-			}
-			}.ToList();
+            var properties = new Property[]{
+            new Property{
+                Name = "Id",
+                Visibility = Visibility.Public,
+                hasGeterAndSeter = true,
+                TypeProperty = "int"
+            }
+            }.ToList();
 
-			properties.AddRange(propertiesFromJson);
+            properties.AddRange(propertiesFromJson);
+            var heritage = new string[] { "IEntityViewModel" }.ToImmutableList();
 
-			var extraDependencies = new List<string>();
-			var imports = new List<string>();
-			imports = GenExtraDependencies(modelJson, ref extraDependencies);
+            var extraDependencies = new List<string>();
+            var imports = new List<string>();
+            imports = GenExtraDependencies(modelJson, ref extraDependencies);
+            imports.Add("Entities.Interface");
+
+            var result = _codeGenerator
+                .ClassGenerator
+                .CreateClass(imports.ToImmutableList(), $"{modelName}ViewModel", "Entities.ViewModels", properties.ToImmutableList(), heritage);
+            return result;
+
+        }
+
+        private FileCode GenerateViewModelUpdateOrNew(string modelName, bool update)
+        {
+            ModelJson modelJson = GetContentJsonFile(modelName);
+
+            var propertiesFromJson = modelJson.Model!.Fields!.Where(x => x.ViewModelVisibility).Select(field =>
+            {
+                return ProcessField(field);
+            }).ToList();
+
+            var properties = Array.Empty<Property>().ToList();
+
+            if (update)
+            {
+                properties.Add(
+                new Property
+                {
+                    Name = "Id",
+                    Visibility = Visibility.Public,
+                    hasGeterAndSeter = true,
+                    TypeProperty = "int"
+                });
+            }
+
+            var heritage = new string[] { "IEntityViewModel" }.ToImmutableList();
+
+            properties.AddRange(propertiesFromJson);
+
+            var extraDependencies = new List<string>();
+            var imports = new List<string>();
+            imports = GenExtraDependencies(modelJson, ref extraDependencies);
+            imports.Add("Entities.Interface");
+
+            var result = _codeGenerator
+                .ClassGenerator
+                .CreateClass(imports.ToImmutableList(), $"{modelName}{((update) ? "Update" : "New")}ViewModel", "Entities.ViewModels", properties.ToImmutableList(), heritage);
+            return result;
+
+        }
+
+        private static List<string> GenExtraDependencies(ModelJson modelJson, ref List<string> extraDependencies)
+        {
+            List<string> imports;
+            if (modelJson.Model!.Dependencies != null)
+            {
+                extraDependencies = modelJson.Model.Dependencies.Select(x => x.Package).ToList()!;
+                imports = new string[] {
+                    "System.ComponentModel",
+                    "System.ComponentModel.DataAnnotations",
+                    "System.ComponentModel.DataAnnotations.Schema",
+                    "Entities.Models",
+                }.ToList();
+                imports.AddRange(extraDependencies);
 
 
-			var result = _codeGenerator
-				.ClassGenerator
-				.CreateClass(imports.ToImmutableList(), $"{modelName}ViewModel", "Entities.ViewModels", properties.ToImmutableList());
-			return result;
+            }
+            else
+            {
+                imports = new string[] {
+                    "System.ComponentModel",
+                    "System.ComponentModel.DataAnnotations",
+                    "System.ComponentModel.DataAnnotations.Schema",
+                    "Entities.Models",
+                }.ToList();
+            }
 
-		}
+            return imports;
+        }
 
-		private FileCode GenerateViewModelUpdateOrNew(string modelName, bool update)
-		{
-			ModelJson modelJson = GetContentJsonFile(modelName);
+        private FileCode GeneraBasicModelCode(string modelName)
+        {
+            ModelJson modelJson = GetContentJsonFile(modelName);
 
-			var propertiesFromJson = modelJson.Model!.Fields!.Where(x => x.ViewModelVisibility).Select(field =>
-			{
-				return ProcessField(field);
-			}).ToList();
+            var propertiesFromJson = modelJson.Model!.Fields!.Select(field =>
+            {
+                return ProcessField(field);
+            }).ToList();
 
-			var properties = new Property[]{
-			new Property{
-				Name = "Id",
-				Visibility = Visibility.Public,
-				hasGeterAndSeter = true,
-				TypeProperty = "int"
-			}
-			}.ToList();
+            var properties = new Property[]{
+            new Property{
+                Name = "Id",
+                Visibility = Visibility.Public,
+                hasGeterAndSeter = true,
+                TypeProperty = "int"
+            }
+            }.ToList();
 
-			properties.AddRange(propertiesFromJson);
+            properties.AddRange(propertiesFromJson);
 
-			var extraDependencies = new List<string>();
-			var imports = new List<string>();
-			imports = GenExtraDependencies(modelJson, ref extraDependencies);
+            var extraDependencies = new List<string>();
+            var imports = new List<string>();
+            imports = GenExtraDependencies(modelJson, ref extraDependencies);
+            imports.Add("Entities.Interface");
 
-			var result = _codeGenerator
-				.ClassGenerator
-				.CreateClass(imports.ToImmutableList(), $"{modelName}{((update) ? "Update" : "New")}ViewModel", "Entities.ViewModels", properties.ToImmutableList());
-			return result;
+            var inharitance = new string[] {
+            "IEntity"
+            }.ToImmutableList();
 
-		}
+            var result = _codeGenerator
+                .ClassGenerator
+                .CreateClass(imports.ToImmutableList(), modelName, "Entities.Models", properties.ToImmutableList(), inharitance);
+            return result;
 
-		private static List<string> GenExtraDependencies(ModelJson modelJson, ref List<string> extraDependencies)
-		{
-			List<string> imports;
-			if (modelJson.Model!.Dependencies != null)
-			{
-				extraDependencies = modelJson.Model.Dependencies.Select(x => x.Package).ToList()!;
-				imports = new string[] {
-					"System.ComponentModel",
-					"System.ComponentModel.DataAnnotations",
-					"System.ComponentModel.DataAnnotations.Schema",
-					"Entities.Models",
-				}.ToList();
-				imports.AddRange(extraDependencies);
-
-
-			}
-			else
-			{
-				imports = new string[] {
-					"System.ComponentModel",
-					"System.ComponentModel.DataAnnotations",
-					"System.ComponentModel.DataAnnotations.Schema",
-					"Entities.Models",
-				}.ToList();
-			}
-
-			return imports;
-		}
-
-		private FileCode GeneraBasicModelCode(string modelName)
-		{
-			ModelJson modelJson = GetContentJsonFile(modelName);
-
-			var propertiesFromJson = modelJson.Model!.Fields!.Select(field =>
-			{
-				return ProcessField(field);
-			}).ToList();
-
-			var properties = new Property[]{
-			new Property{
-				Name = "Id",
-				Visibility = Visibility.Public,
-				hasGeterAndSeter = true,
-				TypeProperty = "int"
-			}
-			}.ToList();
-
-			properties.AddRange(propertiesFromJson);
-
-			var extraDependencies = new List<string>();
-			var imports = new List<string>();
-			imports = GenExtraDependencies(modelJson, ref extraDependencies);
-			imports.Add("Entities.Interface");
-
-			var inharitance = new string[] {
-			"IEntity"
-			}.ToImmutableList();
-
-			var result = _codeGenerator
-				.ClassGenerator
-				.CreateClass(imports.ToImmutableList(), modelName, "Entities.Models", properties.ToImmutableList(), inharitance);
-			return result;
-
-		}
-	}
+        }
+    }
 }
--- Services/Commands/VersionService.cs
+++ Services/Commands/VersionService.cs
@@ -1,22 +1,21 @@
-using System.Reflection;
 using Contracts.Interfaces;
 using Spectre.Console;
 
 namespace Services.Commands
 {
     [AddService]
-	public class VersionService : IVersionService
-	{
-		public int Execute(string[] args)
-		{
-			string version = "1.0.8-alpha";
-			AnsiConsole.Write(
-	            new FigletText("CADMO-CLI")
-		            .LeftAligned()
-		            .Color(Color.LightSkyBlue1));
+    public class VersionService : IVersionService
+    {
+        public int Execute(string[] args)
+        {
+            string version = "1.1.0-alpha";
+            AnsiConsole.Write(
+                new FigletText("CADMO-CLI")
+                    .LeftAligned()
+                    .Color(Color.LightSkyBlue1));
 
-			AnsiConsole.WriteLine($"Version: {version}");
-			return 1;
-		}
-	}
+            AnsiConsole.WriteLine($"Version: {version}");
+            return 1;
+        }
+    }
 }
